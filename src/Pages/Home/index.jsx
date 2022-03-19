e359529b388668576b4aa9eeff32b2e2b36f9e04
Malvino Austin Tanura <malvinoaustintanura@gmail.com>
2022-03-19 13:37:18 +0700

    Mengupload tugas React Hook

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,15 @@
+/** Components */
+import Header from "../../Components/Header";
+import TodoList from "../../Components/TodoList";
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <TodoList />
+        </>
+    )
+    
+}
+
+export default Home;
